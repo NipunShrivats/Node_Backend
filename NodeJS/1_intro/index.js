84fb5b425fdcbafce940b4702623d0e5b0020127
NipunShrivats <nipunrawat16@gmail.com>
2025-08-12 13:30:16 +0530

    changes on Aug12, change no.1

---
+++ NodeJS/1_intro/index.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+const arr = [1, 2, 3, 4];
+console.log(arr);
+
+setTimeout(() => {
+  console.log("this message is delayed by 2 sec");
+}, 2000);
+
+console.log(arr);
