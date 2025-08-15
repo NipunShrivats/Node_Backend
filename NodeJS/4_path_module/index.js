6251da4b34415b10dfda2d65bcff206ed73f3a59
NipunShrivats <nipunrawat16@gmail.com>
2025-08-15 12:43:18 +0530

    changes on Aug15, change no.1

--- NodeJS/4_path_module/index.js
+++ NodeJS/4_path_module/index.js
@@ -3,3 +3,10 @@
 const path = require("path");
 console.log("__filename:", __filename);
 console.log("Directory name:", path.dirname(__filename));
+
+console.log("File name:", path.basename(__filename));
+
+const joinPath = path.join("/user", "documents", "node", "projects");
+console.log(joinPath);
+
+console.log(path.resolve("user", "documents", "node", "projects"));
